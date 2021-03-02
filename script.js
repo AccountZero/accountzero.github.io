3f0d7d620c3129531c83657400037265673791f4
Hubert <hubert.korona@gmail.com>
2021-03-02 20:12:50 +0100

    added an analog clock

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 document.querySelector(".theme-toggle").addEventListener( 'click', () =>{
   document.body.classList.toggle('ruby-theme')
-})
+});
+
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+setInterval(()=> {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+
+  let hour = document.querySelector("#hour");
+  let minutes = document.querySelector("#minutes");
+  let seconds = document.querySelector("#seconds");
+
+  let h = day.getHours();
+  let m = day.getMinutes();
+  let s = day.getSeconds();
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  hour.innerHTML = h+":";
+  minutes.innerHTML = m+":";
+  seconds.innerHTML = s;
+});
